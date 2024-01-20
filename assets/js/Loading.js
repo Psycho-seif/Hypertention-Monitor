d01bd3d45f5b00ba1cc61dbfbfadd5b7cdbb8ee6
Seif Abdelhamid <seifwaelabdelhamid@gmail.com>
2024-01-20 08:59:19 +0200

    Add files via upload

---
+++ assets/js/Loading.js
@@ -0,0 +1,4 @@
+        // Wait for 6 seconds (6000 milliseconds) and then redirect to loginpage.html
+        setTimeout(function() {
+          window.location.href = 'Login.html';
+      }, 6000);
